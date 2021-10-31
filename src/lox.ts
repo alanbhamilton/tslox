505f2cf892487722856bd10cd56b243e77eba087
Alan Hamilton <alan.b.hamilton@gmail.com>
2021-10-31 11:21:05 -0400

    Clean up

--- src/lox.ts
+++ src/lox.ts
@@ -1,16 +1,15 @@
 import {runFile, runPrompt} from './run'
 
-let yargs: string[] = process.argv.slice(2)
+let args: string[] = process.argv.slice(2)
 
 const ERROR_EXIT = 64
-let hadError = false
 
-if (yargs.length > 1) {
+if (args.length > 1) {
   console.log('Usage: tslox [script]')
   process.exit(ERROR_EXIT)
-} else if (yargs.length === 1) {
+} else if (args.length === 1) {
   try {
-    runFile(yargs[0])
+    runFile(args[0])
   } catch(error) {
     console.error(error)
     process.exit(ERROR_EXIT)
