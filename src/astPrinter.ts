c75a7cf887a0d181a4d66df176e06cca8df64eb4
Alan Hamilton <alan.b.hamilton@gmail.com>
2021-10-31 16:02:26 -0400

    Add astPrinter

---
+++ src/astPrinter.ts
@@ -0,0 +1,32 @@
+import * as Expr from './expr'
+
+export class AstPrinter implements Expr.IVisitor<String> {
+  print(expr: Expr.Expr): string {
+    return expr.accept(this)
+  }
+
+  public visitBinaryExpr(expr: Expr.Binary ): string {
+    return this.parenthesize(expr.operator.lexeme, expr.left, expr.right)
+  }
+
+  public visitGroupingExpr(expr: Expr.Grouping): string {
+    return this.parenthesize('group', expr.expression)
+  }
+
+  public visitLiteralExpr(expr: Expr.Literal): string {
+    if (expr.value === null) return 'nil'
+    return expr.value.toString()
+  }
+
+  public visitUnaryExpr(expr: Expr.Unary): string {
+    return this.parenthesize(expr.operator.lexeme, expr.right)
+  }
+
+  public parenthesize(name: string, ...exprs: Expr.Expr[]) {
+    let result = `(${name}`
+
+    exprs.forEach(expr => result += ` ${expr.accept(this)}`)
+    result += ')'
+    return result
+  }
+}
---
+++ tools/test-ast-print.ts
@@ -0,0 +1,16 @@
+import * as Expr from '../src/expr'
+import { Token, TokenType } from '../src/types'
+import { AstPrinter } from '../src/astPrinter'
+
+
+const expression = new Expr.Binary(
+  new Expr.Unary(
+    new Token(TokenType.MINUS, '-', null, 1),
+    new Expr.Literal(123)
+  ),
+  new Token(TokenType.STAR, '*', null, 1),
+  new Expr.Grouping(
+    new Expr.Literal(45.67))
+  )
+
+console.log(new AstPrinter().print(expression))
